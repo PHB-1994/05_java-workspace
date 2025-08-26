06295676a7aca9413c0bc82e437fc55a308bb767
PHB-1994 <gudqls0991@naver.com>
2025-08-26 13:18:07 +0900

    Array 배열

---
+++ src/edu/array/ArrayRun.java
@@ -0,0 +1,14 @@
+package edu.array;
+
+import java.util.Scanner;
+
+/**
+ * 배열 실행용 클래스
+ */
+public class ArrayRun {
+    // main 메서드로 반드시 실행할 구문
+    public static void  main(String args[]) {
+        ArrayEx1 ex1 = new ArrayEx1();
+        ex1.method1();
+    }
+}
