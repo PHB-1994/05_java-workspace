fd258469c2a4af13fdb46a20ec22120677c74307
PHB-1994 <gudqls0991@naver.com>
2025-08-29 09:22:59 +0900

    day3 과제 실습

---
+++ src/edu/practice/day3/Car.java
@@ -0,0 +1,166 @@
+package edu.practice.day3;
+
+public class Car {
+
+
+    private String brand;               // 브랜드
+    private String model;               // 모델명
+    private int fuel;                   // 연료랑, 0 ~ 100
+    private int speed = 0;                  // 현재 속도, 기본값 0
+    private boolean isEngineOn = false; // 엔진 상태, 기본값 false
+    private int totalDistance = 0;          // 총 주행거리, 기본값 0
+
+    // 기본 생성자
+    public Car() {
+    }
+
+    // 필수 생성자
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        this.fuel = 100;
+    }
+
+    // setter
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setEngineOn(boolean engineOn) {
+        isEngineOn = engineOn;
+    }
+
+    public void setTotalDistance(int totalDistance) {
+        this.totalDistance = totalDistance;
+    }
+
+    // getter
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isEngineOn() {
+        return isEngineOn;
+    }
+
+    public int getTotalDistance() {
+        return totalDistance;
+    }
+
+    /**
+     * 차 정보
+     */
+    public void carInfo(){
+        System.out.println("=== 자동차 정보 ===");
+        System.out.println("브랜드 : " + getBrand());
+        System.out.println("모델 : " + getModel());
+        System.out.println("연료량 : " + getFuel() + "L");
+        System.out.println("속도 : " + getSpeed() + "km/h");
+        System.out.println("엔진상태 : " + isEngineOn());
+        System.out.println("총 주행거리 : " + getTotalDistance() + "km");
+    }
+
+    /**
+     * 엔진 시동
+     */
+    public void startEngine(){
+        if(isEngineOn){
+            System.out.println("이미 시동이 걸려있습니다.");
+        }else {
+            System.out.println(getModel() + " 엔진이 시동되었습니다.");
+            isEngineOn = true;
+        }
+        if(fuel == 0){
+            System.out.println("연료가 부족하여 시동을 걸 수 없습니다.");
+            stopEngine();
+        }
+    }
+
+    /**
+     * 엔진 정지
+     */
+    public void stopEngine(){
+        System.out.println(getModel() + " 엔진이 정지되었습니다.");
+        isEngineOn = false;
+    }
+
+    /**
+     * 가속
+     * @param speedUp 올릴 속도
+     */
+    public void accelerate(int speedUp){
+        if(!isEngineOn){
+            System.out.println("엔진이 꺼져있어 가속할 수 없습니다.");
+        }
+
+        if(fuel == 0){
+            System.out.println("연료가 없어 가속할 수 없습니다.");
+        }else {
+            System.out.println(speedUp + "km/h로 가속했습니다. 현재 속도: " + speedUp + "km/h");
+            speed += speedUp;
+        }
+    }
+
+    /**
+     * 감속
+     * @param speedDown 내릴 속도
+     */
+    public void brake(int speedDown){
+        speed -= speedDown;
+        System.out.println(speedDown + "km/h로 감속했습니다. 현재 속도: " + speed + "km/h");
+    }
+
+    /**
+     * 주유
+     * @param amount 주유할 연료량
+     */
+    public void refuel(int amount){
+        fuel += amount;
+        System.out.println(amount + "L 주유했습니다. 현재 연료량: " + fuel + "L");
+    }
+
+    /**
+     * 주행
+     * @param distance 주행할 거리
+     */
+    public void drive(int distance){
+        int fuelNeeded = distance / 10; // 10km당 1L 소모
+        if (fuel < fuelNeeded) {
+            System.out.println("연료가 부족하여 " + distance + "km를 주행할 수 없습니다.");
+            return;
+        }else {
+            System.out.println(distance + "km 주행했습니다. 연료 " + fuelNeeded + "L 소모되었습니다.");
+            totalDistance += distance;
+            fuel -= fuelNeeded;
+        }
+
+        if(fuel == 0){
+            System.out.println("연료가 모두 소모되었습니다. 엔진을 정지합니다.");
+            isEngineOn = false;
+        }
+    }
+}
--- src/edu/practice/day3/PracticeRun.java
+++ src/edu/practice/day3/PracticeRun.java
@@ -6,8 +6,8 @@ public class PracticeRun {
         Practice2 p2 = new Practice2();
         Practice3 p31 = new Practice3(100000,"1234");
         Practice3 p32 = new Practice3(0,"5678");
-        Practice4 p4 = new Practice4();
-        Practice5 p5 = new Practice5();
+        Student p4 = new Student();
+        Car p5 = new Car();
 
         // 원하는 문제의 주석을 해제하고 실행하세요
         System.out.println("=== 문제 1: 도서 관리 클래스 ===");
@@ -50,7 +50,41 @@ public class PracticeRun {
         System.out.println("\n=== 문제 4: 학생 성적 관리 시스템 ===");
         // p4.method4();
 
+//        p4.setName("홍길동");
+//        p4.setStudentId(20241001);
+//        p4.inputScores();
+//        p4.getTotalScore();
+//        p4.printReport();
+
+
         System.out.println("\n=== 문제 5: 자동차 관리 시스템 ===");
         //p5.method5();
+        Car car = new Car("현대", "아반떼");
+        car.setFuel(100);
+        car.setSpeed(0);
+        car.setEngineOn(false);
+        car.setTotalDistance(0);
+        car.carInfo();
+        System.out.println();
+        car.startEngine();
+        System.out.println();
+        car.accelerate(50);
+        System.out.println();
+        car.drive(500);
+        System.out.println();
+        car.carInfo();
+        System.out.println();
+        car.refuel(32);
+        System.out.println();
+        car.brake(10);
+        System.out.println();
+        car.carInfo();
+        System.out.println();
+        car.stopEngine();
+        System.out.println();
+        car.startEngine();
+        System.out.println();
+        car.accelerate(50);
+        car.drive(820);
     }
 }
