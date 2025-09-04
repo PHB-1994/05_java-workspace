4d0d0e76ceb6d5d0aa41abba7899dda7f4a36c86
PHB-1994 <gudqls0991@naver.com>
2025-09-04 17:29:04 +0900

    컬렉션 (List - ArrayList)

--- src/edu/collection/pack3/run/MapRun.java
+++ src/edu/collection/pack3/run/MapRun.java
@@ -6,5 +6,6 @@ public class MapRun {
     public static void main(String[] args){
         MapService mapService = new MapService();
         mapService.method1();
+        mapService.method2();
     }
 }
