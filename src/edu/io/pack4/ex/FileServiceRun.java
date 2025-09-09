bb42317ce08d47adaa9ec9e7b8451069156194a3
PHB-1994 <gudqls0991@naver.com>
2025-09-09 13:16:35 +0900

    IO(입출력)

---
+++ src/edu/io/pack4/ex/FileServiceRun.java
@@ -0,0 +1,11 @@
+package edu.io.pack4.ex;
+
+import java.io.IOException;
+
+public class FileServiceRun {
+
+    public static void main(String[] args){
+        FileWriteService fws =new FileWriteService();
+        fws.method();
+    }
+}
