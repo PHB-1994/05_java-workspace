ff7d2619acd64cc2e31709188c832b6912efbbb0
PHB-1994 <gudqls0991@naver.com>
2025-09-10 11:14:29 +0900

    IO(입출력) - 프로필 사진 저장하기

--- src/edu/io/pack5/run/프로필사진run.java
+++ src/edu/io/pack5/run/프로필사진run.java
@@ -8,7 +8,16 @@ public class 프로필사진run {
 
         프로필사진service service = new 프로필사진service();
 
-        service.createProfileFolder();
+//        service.createProfileFolder("member03","프로필사진2.png");
+//        service.createProfileFolder("member04","프로필사진2.png");
 
+        String imgUrl = "https://cdn.dailysecu.com/news/photo/202104/123449_145665_1147.png";
+
+//        service.saveImg(imgUrl);
+
+        // 인터넷에서 이미지 주소를 복사한 후
+        // 원하는 이미지를
+        String imgUrl2 = "이미지 주소 넣기";
+        service.saveImg2(imgUrl2);
     }
 }
