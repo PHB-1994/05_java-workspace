2d048860a06093b3a0c03cf621bbee5b3eb94089
PHB-1994 <gudqls0991@naver.com>
2025-09-12 12:20:15 +0900

    IO(String, StringBuilder)

---
+++ src/edu/io/pack8/RealTimeWriterService.java
@@ -0,0 +1,13 @@
+package edu.io.pack8;
+
+public class RealTimeWriterService {
+
+    public void method1(){
+
+        String result = "";
+
+        for(int i = 0; i < 1000; i++){
+            result += "숫자 : " + i + "\n";
+        }
+    }
+}
