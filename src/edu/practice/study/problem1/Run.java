030bf596249a60645cd8f80b1d8ce0d3b833a210
PHB-1994 <gudqls0991@naver.com>
2025-09-16 18:03:36 +0900

    스터디 1주차 1번과제

---
+++ src/edu/practice/study/problem1/Run.java
@@ -0,0 +1,12 @@
+package edu.practice.study.problem1;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        School s = new School();
+
+        s.addStudent();
+    }
+}
+
